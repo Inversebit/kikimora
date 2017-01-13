289f4399ccbbc64d375950f67e394fd2f13cab51
Inversebit <inversebit@gmail.com>
2017-01-13 22:50:02 +0100

    Created server to modify URL to check - Noticed that kikimora doesn't work on secure pages

--- kikimora.ino
+++ kikimora.ino
@@ -11,8 +11,8 @@
 
  /* IMPORTS */
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
 
 extern "C" {
   #include "user_interface.h"
@@ -27,20 +27,106 @@ const int rled = 0;
 //Timer
 os_timer_t myTimer;
 
-//WiFi thingies
-ESP8266WiFiMulti WiFiMulti;
+//Server vars
+ESP8266WebServer server(80);
 
-//HTPP thingies
-HTTPClient http;
-String dest_page = "http://192.168.0.1/";
-//String dest_page = "https://google.com";
-//String cert_fingerprint = "10 62 AA 33 06 6F D4 B2 00 F2 C9 B4 C7 B0 ED 39 46 03 0D EB";
+//HTPPClient vars
+HTTPClient httpClient;
+String dest_page = "";
+String cert_fingerprint = "";
 
 //Control vars
 bool shouldPing = false;
 
+//Simple page
+const char PROGMEM webpageRoot[] = "\
+<!DOCTYPE html>\
+<html>\
+  <body>\
+    <h1>Kikimora</h1>\
+    <h2>ESP8266-based Systems Monitoring Service</h2>\
+    <p>Please enter URL to monitor. In case it's an HTTPS webpage do also indicate the page certificate's SHA1 fingerprint below.</p>\
+    <form action=\"/submit\" method=\"POST\">\
+      <fieldset>\
+        <legend>Monitoring target information:</legend>\
+        URL to monitor:<br>\
+        <input type=\"text\" name=\"url\" value=\"@@current_url\" autofocus required/><br><br>\
+        Cert SHA1 fingerprint:<br>\
+        <input type=\"text\" name=\"fingerprint\" value=\"@@current_fingerprint\"/><br><br>\
+        <input type=\"submit\" value=\"Submit\">\
+      </fieldset>\
+    </form>\
+  </body>\
+</html>";
+
+const char PROGMEM webpageForm[] = "\
+<!DOCTYPE html>\
+<html>\
+  <body>\
+    <h1>Kikimora</h1>\
+    <h2>ESP8266-based Systems Monitoring Service</h2>\
+    <p>@@message</p>\
+  </body>\
+</html>";
 
 /* SETUP FUNCTIONS */
+//SERVER FUNCTIONS
+void handleAccess() 
+{
+  String msg = String(webpageRoot);
+  msg.replace("@@current_url", dest_page);
+  msg.replace("@@current_fingerprint", cert_fingerprint);
+  server.send(200, "text/html", msg);
+}
+
+void handleForm()
+{
+  bool formOk = false;
+  String temp_dest = "";
+  String temp_fingerp = "";
+  
+  if (server.args() > 0 ) {
+    for ( uint8_t i = 0; i < server.args(); i++ ) {
+      if (server.argName(i) == "url") {
+        temp_dest = server.arg(i);
+      }
+      else if(server.argName(i) == "fingerprint")
+      {
+        temp_fingerp = server.arg(i);
+      }
+    }
+  }
+
+  if(temp_dest.length() > 0)
+  {
+    dest_page = temp_dest;
+    cert_fingerprint = temp_fingerp;
+    formOk = true;
+  }
+
+  String res = "Info <b>NOT</b> saved. An error has occurred.";
+  if(formOk)
+  {
+    res = "Info <b>SUCCESSFULLY</b> saved. <a href=\"/\">Return to index</a>.";
+  }
+  
+  String msg = String(webpageForm);
+  msg.replace("@@message", res);
+  server.send(200, "text/html", msg);
+}
+
+void handleNotFound()
+{
+  server.send(404, "text/plain", "");
+}
+
+void setupServer()
+{
+  server.on("/", handleAccess);
+  server.on("/submit", handleForm);
+  server.onNotFound(handleNotFound);
+  server.begin();
+}
 
 //TIMER FUNCTIONS
 void timerCallback(void *pArg) {
@@ -49,7 +135,7 @@ void timerCallback(void *pArg) {
 
 void user_init(void) {
   os_timer_setfn(&myTimer, timerCallback, NULL);
-  os_timer_arm(&myTimer, 15000, true);
+  os_timer_arm(&myTimer, 5000, true);
 }
 
 //OTHER SETUP
@@ -61,7 +147,10 @@ void setupPins()
 
 void setupWiFiConn()
 {
-  WiFiMulti.addAP("SSID", "PASSWORD");
+  WiFi.begin("SSID", "PASSWD");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
 }
 
 //*THE* SETUP
@@ -69,6 +158,7 @@ void setup() {
   setupPins();
   user_init();
   setupWiFiConn();
+  setupServer();
 }
 
 /* LED FUNCTIONS */
@@ -89,7 +179,7 @@ int litPin()
   return -1;
 }
 
-void blinkLEDOff(int ledPin, int times, int blinkDuration)
+void blinkLED(int ledPin, int times, int blinkDuration)
 {
   for(int i = 0; i < times; i++)
   {
@@ -120,17 +210,31 @@ bool pingWeb()
 {
   bool res = false;
   
-  //http.begin(dest_page, cert_fingerprint);
-  http.begin(dest_page);
+  if(dest_page.length() == 0)
+  {
+    blinkLED(rled, 5, 100);
+  }
   
-  int httpCode = http.GET();
+  if(cert_fingerprint.length() == 0)
+  {
+    //HTTP Conn
+    httpClient.begin(dest_page);  
+  }
+  else
+  {
+    //HTTPS Conn
+    httpClient.begin(dest_page, cert_fingerprint);
+    httpClient.begin(dest_page);
+  }
+  
+  int httpCode = httpClient.GET();
   if(httpCode > 0) {
     if(httpCode == HTTP_CODE_OK) {
       res = true;
     }
   }
-  
-  http.end();
+
+  httpClient.end();
 
   return res;
 }
@@ -138,15 +242,14 @@ bool pingWeb()
 /* (THE REST OF THE) FUNCTIONS */
 void loop() 
 {
-  if((WiFiMulti.run() == WL_CONNECTED)) 
+  server.handleClient();
+  
+  if (shouldPing == true)
   {
-    if (shouldPing == true)
-    {
-      blinkLEDOff(litPin(), 2, 250);
-      bool isUp = pingWeb();
-      showStatus(isUp);
-      shouldPing = false;
-    }
+    blinkLED(litPin(), 2, 250);
+    bool isUp = pingWeb();
+    showStatus(isUp);
+    shouldPing = false;
   }
    
   yield();  // or delay(0);
